d8d5cfd609a2242d3a4141b438be71094bddac42
Mahantesh Hiremath <mahanteshimath@gmail.com>
2025-05-13 04:37:24 +0530

    Refactor selected company display format in Fundamental Analysis page

--- pages/Fundamental_Analysis.py
+++ pages/Fundamental_Analysis.py
@@ -15,7 +15,8 @@ try:
     if selected_company:
         company_info = nse_data[nse_data["NAME OF COMPANY"] == selected_company].iloc[0]
         symbol = company_info["SYMBOL"]
-        st.write(f"**Selected Company:** {symbol}: {selected_company}")
+        Listed_Name=f"{symbol}:{selected_company}"
+        st.write(f"**Selected Company:** {Listed_Name}")
 
 except Exception as e:
     st.error(f"Failed to load NSE data: {str(e)}")
