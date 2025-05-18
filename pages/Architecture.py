04411fb47ae60836463d874c232c1265e5716f82
Mahantesh Hiremath <mahanteshimath@gmail.com>
2025-05-18 18:59:14 +0530

    components.html(html_content, width=900, height=800)

--- pages/Architecture.py
+++ pages/Architecture.py
@@ -7,8 +7,7 @@ import streamlit.components.v1 as components
 with open("./src/Architecture_app.txt", "r") as file:
     html_content = file.read()
 
-components.html(html_content, width=800, height=600, scrolling=True)
-
+components.html(html_content, width=900, height=800)
 
 st.markdown(
     '''
